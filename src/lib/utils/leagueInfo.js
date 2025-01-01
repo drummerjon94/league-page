3bc3877bcb359f9e66d50c0b28cc16ae9cf7e11e
drummerjon94 <jonny-evans@outlook.com>
2025-01-01 23:27:33 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -131,6 +131,28 @@ export const managers = [
        "tradingScale": 7, // 1 - 10
        "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      },
+     {
+       "managerID": "609045409725210624",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+       "name": "Stefan",
+       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+       "location": "Sweden", // (optional)
+       "bio": "Swedish millionaire. Ruthless. Will fight to the death",
+       "photo": "/managers/everyone.png", // square ratio recommended (no larger than 500x500)
+       "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
+       "favoriteTeam": "tb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+       "rival": {
+         name: "Ross", // Can be anything (usually your rival's name)
+         link: 2, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+       },
+       "favoritePlayer": 2216, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
+       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
+       "philosophy": "The older the better.",
+       "tradingScale": 8, // 1 - 10
+       "preferredContact": "WhatsApp", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+     },
     // {
     //   "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     //   "name": "Your Name",
