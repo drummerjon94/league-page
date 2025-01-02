19eb033f07d9ba31f48ec55a1488b33b5619c970
drummerjon94 <jonny-evans@outlook.com>
2025-01-02 11:04:18 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -188,7 +188,7 @@ export const managers = [
        "location": "England", // (optional)
        "bio": "Member since day 1. Always a contender.",
        "photo": "/managers/everyone.png", // square ratio recommended (no larger than 500x500)
-       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Age", // Can be anything (usually your rival's name)
