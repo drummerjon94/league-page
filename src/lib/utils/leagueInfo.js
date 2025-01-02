90a2be9d21034d36e8f9fa2b5621cabe292f5be1
drummerjon94 <jonny-evans@outlook.com>
2025-01-02 12:00:26 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,7 +27,7 @@ export const managers = [
        "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "England", // (optional)
        "bio": "The baddest manager in town. Quite literally.",
-       "photo": "/managers/jonny2.png", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/jonny3.png", // square ratio recommended (no larger than 500x500)
        "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
