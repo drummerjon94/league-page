49998afdb1223eb7001440290f783f3141832101
drummerjon94 <jonny-evans@outlook.com>
2025-01-02 11:43:56 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -195,7 +195,7 @@ export const managers = [
          link: null, // manager array number within this array, or null to link back to all managers page
          image: "/managers/age.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
-       "favoritePlayer": 4046, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "favoritePlayer": 232, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
        "philosophy": "You don't win anything with kids.",
        "tradingScale": 4, // 1 - 10
