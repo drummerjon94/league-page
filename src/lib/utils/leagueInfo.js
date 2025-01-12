2d8ce0a18d52ba66c7048ef176c25b1ba48d7162
drummerjon94 <jonny-evans@outlook.com>
2025-01-12 19:12:33 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -202,23 +202,23 @@ export const managers = [
        "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      },
      {
-       "managerID": "1156248882016407552",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-       "name": "Benjamin",
+       "managerID": "1014582897908596736",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+       "name": "Jamie",
        "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Italy", // (optional)
-       "bio": "New kid on the block. Joined the league in 2024.",
-       "photo": "/managers/ben.png", // square ratio recommended (no larger than 500x500)
+       "location": "England", // (optional)
+       "bio": "New kid on the block. Joined the league in 2025.",
+       "photo": "/managers/jamie.png", // square ratio recommended (no larger than 500x500)
        "favoriteTeam": "min", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "Roster Management", // Can be anything (usually your rival's name)
-         link: null, // manager array number within this array, or null to link back to all managers page
-         image: "/managers/roster.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         name: "Charlie", // Can be anything (usually your rival's name)
+         link: 3, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/charlie.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
        },
-       "favoritePlayer": 6794, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "favoritePlayer": 5045, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-       "philosophy": "You can't be disappointed if you don't set a lineup.",
-       "tradingScale": 1, // 1 - 10
+       "philosophy": "TBC.",
+       "tradingScale": 5, // 1 - 10
        "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      },
   ]
